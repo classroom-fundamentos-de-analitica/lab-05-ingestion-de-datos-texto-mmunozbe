41f36b0d408930172a3883ced0859e1c3e4d49fc
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-02-28 21:03:36 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import pandas as pd
 if not os.path.exists("train_dataset.csv"):
     raise FileNotFoundError("File 'train_dataset.csv' not found")
 
-train_dataset = pd.read_csv("train_dataset.txt")
+train_dataset = pd.read_csv("train_dataset.csv")
 
 assert train_dataset.columns[0] == "phrase"
 assert train_dataset.columns[1] == "sentiment"
@@ -19,7 +19,7 @@ assert counts["positive"] == 458
 assert counts["negative"] == 236
 
 
-if not os.path.exists("test_dataset.txt"):
+if not os.path.exists("test_dataset.csv"):
     raise FileNotFoundError("File 'test_dataset.csv' not found")
 
 test_dataset = pd.read_csv("test_dataset.csv")
