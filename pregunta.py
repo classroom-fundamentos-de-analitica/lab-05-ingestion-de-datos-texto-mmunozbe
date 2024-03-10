e209cb392f8f7ba516074632cfe4ed4306787f09
Matheo Muñoz Betancur <131404918+mmunozbe@users.noreply.github.com>
2024-03-10 12:24:25 -0500

    :bento:

---
+++ pregunta.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+def ingest(path: str):
+
+    df = pd.DataFrame(columns=['phrase', 'sentiment'])
+
+    for dirpath, dirname, filenames in os.walk(path):
+
+        for file in filenames:
+
+            if file.endswith('.txt'):
+
+                with open(os.path.join(dirpath, file), 'r') as f:
+
+                    df = df._append({'phrase': f.read(), 
+                                    'sentiment': os.path.basename(dirpath)}, 
+                                    ignore_index=True)
+    
+    df.to_csv(f"{path}_dataset.csv", index=False)
+
+
+
+ingest("test")
+ingest("train")
